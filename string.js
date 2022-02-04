f55d0ba644dc2f1afefc4c4edc6ae22b3e03cbb0
gdwb3219 <gdwb3219@gmail.com>
2022-02-04 16:09:45 +0900

    Each Type of Data in TypeScript

---
+++ string.js
@@ -0,0 +1,7 @@
+"use strict";
+let myName = 'Mark Lee';
+let age = 39;
+let sentence = `Hello, My Name is ${myName}
+
+I'll be ${age + 1} years old next month.`;
+console.log(sentence);
---
+++ string.ts
@@ -0,0 +1,9 @@
+let myName: string = 'Mark Lee';
+
+let age: number = 39;
+
+let sentence: string = `Hello, My Name is ${myName}
+
+I'll be ${ age + 1 } years old next month.`;
+
+console.log(sentence);
